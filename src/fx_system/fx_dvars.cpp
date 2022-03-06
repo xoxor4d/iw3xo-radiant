3623e6349b9838653425b42ec8c75eabb87328e8
xoxor4d <45299104+xoxor4d@users.noreply.github.com>
2022-03-06 01:56:13 +0100

    fix fx dvar namespace, bump version and readme

--- src/fx_system/fx_dvars.cpp
+++ src/fx_system/fx_dvars.cpp
@@ -1,40 +1,43 @@
 #include "std_include.hpp"
 
-namespace fx_system
+namespace dvars
 {
 	game::dvar_s* fx_draw = nullptr;
 	game::dvar_s* fx_enable = nullptr;
 	game::dvar_s* fx_cull_elem_spawn = nullptr;
 	game::dvar_s* fx_cull_elem_draw = nullptr;
 	game::dvar_s* fx_cull_effect_spawn = nullptr;
+}
 
+namespace fx_system
+{
 	void register_dvars()
 	{
-		fx_enable = dvars::register_bool(
+		dvars::fx_enable = dvars::register_bool(
 			/* name		*/ "fx_enable",
 			/* default	*/ true,
 			/* flags	*/ game::dvar_flags::saved,
 			/* desc		*/ "Toggles all effects processing");
 
-		fx_draw = dvars::register_bool(
+		dvars::fx_draw = dvars::register_bool(
 			/* name		*/ "fx_draw",
 			/* default	*/ true,
 			/* flags	*/ game::dvar_flags::saved,
 			/* desc		*/ "Toggles drawing of effects after processing");
 
-		fx_cull_elem_spawn = dvars::register_bool(
+		dvars::fx_cull_elem_spawn = dvars::register_bool(
 			/* name		*/ "fx_cull_elem_spawn",
 			/* default	*/ true,
 			/* flags	*/ game::dvar_flags::none,
 			/* desc		*/ "Culls effect elems for spawning");
 
-		fx_cull_elem_draw = dvars::register_bool(
+		dvars::fx_cull_elem_draw = dvars::register_bool(
 			/* name		*/ "fx_cull_elem_draw",
 			/* default	*/ true,
 			/* flags	*/ game::dvar_flags::saved,
 			/* desc		*/ "Culls effect elems for drawing");
 
-		fx_cull_effect_spawn = dvars::register_bool(
+		dvars::fx_cull_effect_spawn = dvars::register_bool(
 			/* name		*/ "fx_cull_effect_spawn",
 			/* default	*/ false,
 			/* flags	*/ game::dvar_flags::none,
--- src/fx_system/fx_dvars.hpp
+++ src/fx_system/fx_dvars.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-namespace fx_system
+namespace dvars
 {
 	extern	game::dvar_s* fx_draw;
 	extern	game::dvar_s* fx_enable;
 	extern	game::dvar_s* fx_cull_elem_spawn;
 	extern	game::dvar_s* fx_cull_elem_draw;
 	extern	game::dvar_s* fx_cull_effect_spawn;
+}
 
+namespace fx_system
+{
 	void	register_dvars();
-
 }
